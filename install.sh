1bd0514a21dcbbeff81e9afe688dba6406ae5349
weluvsz <134206318+weluvsz@users.noreply.github.com>
2025-07-13 16:36:42 +0700

    install.sh fix

--- install.sh
+++ install.sh
@@ -1,35 +1,41 @@
 #!/bin/bash
 
-echo "installing dotfiles by weluvsz, yay^^\n---"
+echo "installing dotfiles by weluvsz, yay^^"
+echo "---"
 
 
 echo "copying configs"
 
 cp -r ./config/* "$HOME/.config"
 
-echo "---\nconfig copying completed\n---"
+echo "---"
+echo "config copying completed"
+echo "---"
 
 
 if [ ! -d "$HOME/wallpaper" ]; then
-	echo "creating directory  and copying wallpapers(in $HOME/wallpaper)"
-	mkdir -p "$HOME/wallpaper"
-	cp -r ./wallpaper/* "$HOME/wallpaper"
+        echo "creating directory  and copying wallpapers(in $HOME/wallpaper)"
+        mkdir -p "$HOME/wallpaper"
+        cp -r ./wallpaper/* "$HOME/wallpaper"
 else
-	echo "copying wallpapers into $HOME/wallpaper"
-	cp ./wallpaper/* "$HOME/wallpaper"
+        echo "copying wallpapers into $HOME/wallpaper"
+        cp ./wallpaper/* "$HOME/wallpaper"
 fi
 echo "---"
 
 echo "installing fonts"
+if [ ! -d " $HOME/.local/share/fonts" ]; then
+        mkdir "$HOME/.local/share/fonts"
+fi
 
 cp ./fonts/* "$HOME/.local/share/fonts"
 fc-cache -fv
 echo "installed✅"
 
-echo "\n---\ninstalling zshrc"
+echo "---"
+echo "installing zshrc"
 cp .zshrc "$HOME/.zshrc"
 echo "done"
 
-echo "---\ninstalling dotfiles finished"
-
-
+echo "---"
+echo "installing dotfiles finished"
